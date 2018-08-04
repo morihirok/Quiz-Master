f6918387c8716a5c0890bce110d6518dadce61f5
morihirok <omnimani3@gmail.com>
2018-08-04 15:27:11 +0900

    Create quiz mode routing

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,10 @@
 Rails.application.routes.draw do
   root 'home#index'
   resources :questions
+
+  namespace :quiz_mode do
+    resources :questions, only: [:index, :show] do
+      resources :answer, only: :index
+    end
+  end
 end

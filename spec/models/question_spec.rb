56bd32a0003e554e7c51fe9f2a27232359e1df0a
morihirok <omnimani3@gmail.com>
2018-08-04 12:25:12 +0900

    Add presence validation to Question content and answer

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -6,4 +6,16 @@ RSpec.describe Question, type: :model do
   it 'is valid with a content and answer' do
     expect(FactoryBot.build(:question)).to be_valid
   end
+
+  it 'is invalid without content' do
+    question = FactoryBot.build(:question, content: nil)
+    question.valid?
+    expect(question.errors[:content]).to include("can't be blank")
+  end
+
+  it 'is invalid without answer' do
+    question = FactoryBot.build(:question, answer: nil)
+    question.valid?
+    expect(question.errors[:answer]).to include("can't be blank")
+  end
 end

3a79c2320657948155a8d2cd8b6a3afff9d3c8c4
morihirok <omnimani3@gmail.com>
2018-08-05 11:38:29 +0900

    Display random question

--- app/javascript/packs/AnswerApp.jsx
+++ app/javascript/packs/AnswerApp.jsx
@@ -4,23 +4,62 @@
 
 import React from "react";
 import ReactDOM from "react-dom";
-import PropTypes from "prop-types";
 
-const AnswerApp = () => (
-  <div className="p-5">
-    <h4 className="mb-4">Question :</h4>
-    <h2 className="mb-4">How Many vowels are there in the English alphabet?</h2>
-    <form>
-      <div className="form-group">
-        <label htmlFor="answerInput">Answer</label>
-        <input id="answerInput" type="text" className="form-control w-75" />
-        <button className="btn btn-primary mt-3" type="button">
-          Apply!
-        </button>
-        <p className="mt-4">Show Answer</p>
+class AnswerApp extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      question: {
+        id: null,
+        content: null
+      },
+      answer: ""
+    };
+    this.handleOnChangeAnswer = this.handleOnChangeAnswer.bind(this);
+  }
+
+  async componentDidMount() {
+    const resp = await window.fetch(`${window.location.href}.json`);
+    const respJson = await resp.json();
+    this.setState({
+      question: {
+        id: respJson.id,
+        content: respJson.content
+      }
+    });
+  }
+
+  handleOnChangeAnswer({ target }) {
+    this.setState({ answer: target.value });
+  }
+
+  render() {
+    const { question, answer } = this.state;
+
+    return (
+      <div className="p-5">
+        <h4 className="mb-4">Question :</h4>
+        <h2 className="mb-4">{question.content}</h2>
+        <form>
+          <div className="form-group">
+            <label htmlFor="answerInput">Answer</label>
+            <input
+              onChange={this.handleOnChangeAnswer}
+              value={answer}
+              id="answerInput"
+              type="text"
+              className="form-control w-75"
+            />
+            <button className="btn btn-primary mt-3" type="button">
+              Apply!
+            </button>
+            <p className="mt-4">Show Answer</p>
+          </div>
+        </form>
       </div>
-    </form>
-  </div>
-);
+    );
+  }
+}
 
 ReactDOM.render(<AnswerApp />, document.getElementById("AnswerApp"));

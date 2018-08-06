611d128d64ed2886ed113ec723143125946476f2
morihirok <omnimani3@gmail.com>
2018-08-07 00:37:55 +0900

    Add next question link

--- app/javascript/packs/components/QuizResult.jsx
+++ app/javascript/packs/components/QuizResult.jsx
@@ -7,7 +7,12 @@ const QuizResult = ({ result }) => {
     return (
       <div className="alert alert-success w-75 mt-4" role="alert">
         Correct!
-        <strong className="float-right">Next Question</strong>
+        <a
+          className="alert-link float-right"
+          href={`${window.location.origin}/quiz_mode/questions/random`}
+        >
+          Next Question
+        </a>
       </div>
     );
   }
